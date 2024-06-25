8e48dd9efa8adce7ddd10f7b80f319dc59c645d6
Jigmet Dadul <85403349+jigmetdadul@users.noreply.github.com>
2024-06-25 16:59:31 +0530

    added subnet_id and key_name

--- main.tf
+++ main.tf
@@ -5,4 +5,6 @@ provider "aws" {
 resource "aws_instance" "example" {
 	ami= "ami-04b70fa74e45c3917" # Specify an appropriate AMI ID
 	instance_type = "t2.micro"
+	subnet_id = "subnet-0605ec1b5179f7c85"
+	key_name = "TargetServerKP"
 }
